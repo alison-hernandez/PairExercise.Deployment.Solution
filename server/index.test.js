643b16c7bc4fb1026d82350b227fa37c59e904a9
Alison <alison.a.hernandez@gmail.com>
2020-06-11 00:14:44 -0400

    All the dones

--- server/index.test.js
+++ server/index.test.js
@@ -23,8 +23,10 @@ describe('GET /users', () => {
       const res = await request(app).get('/api/users')
       expect(res.status).to.equal(200)
       expect(res.body.length).to.equal(seedUsers.length)
+      done()
     } catch (error) {
       console.log(error);
+      done()
     }
     done()
   })
